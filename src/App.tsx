bc89b89e427df32bbc1106d6eeee2cbae57994f3
reza <rezaayaari.57@gmail.com>
2022-12-17 14:18:22 +0330

    import route from routes.ts file to app.ts and then map throught routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound/NotFound';
-
-const CreateUser = React.lazy(() => import('./pages/UserManagement/CreateUser/CreateUser'))
-const ViewUser = React.lazy(() => import('./pages/UserManagement/ViewUser/ViewUser'))
-const Login = React.lazy(() => import('./pages/Login/Login'))
+import { privateRoutes } from './routes/routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <Route path='/login' element={<Login />} />
-          <Route path='/create-user' element={<CreateUser />} />
-          <Route path='/view-user' element={<ViewUser />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
+        {privateRoutes.map(({ path, Component }) => {
+          return <Route key={path} path={path} element={<Component />} />;
+        })}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
